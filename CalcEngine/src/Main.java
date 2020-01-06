791521a7988b09c8261f92c585efe556b9320e94
The_Tenth <joseph.justin.diaz@gmail.com>
2020-01-06 10:57:29 -0500

    Second version (I think) of the pluralsight calEngine

---
+++ CalcEngine/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] result = new double[opCodes.length];
+
+        for (int i = 0; i < opCodes.length; i++) {
+            if (opCodes[i] == 'a')
+                result[i] = leftVals[i] + rightVals[i];
+            else if (opCodes[i] == 's')
+                result[i] = leftVals[i] - rightVals[i];
+            else if (opCodes[i] == 'd') {
+                result[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+            } else if (opCodes[i] == 'm')
+                result[i] = leftVals[i] * rightVals[i];
+            else {
+                System.out.println("Error - invalid opCode");
+                result[i] = 0.0d;
+            }
+
+        }
+        for (double theResult : result) {
+
+            System.out.println(theResult);
+
+        }
+
+
+    }
+}
